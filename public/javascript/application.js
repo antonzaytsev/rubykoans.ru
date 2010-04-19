e27e098ade64f3a40117031b8c626d7a73cc51e8
Jerry Nummi <jerry@theedgecase.com>
2010-04-19 11:21:51 -0400

    More styling; Refactoring.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,7 +1,27 @@
 $(function(){
+  document.createElement('header');
+  document.createElement('section');
+
+  $('body').append('<div id="top_line"></div>');
+
   $('a[rel=external]').click(function() {
     var newWindow = window.open($(this).attr('href'), '_blank');
     if(newWindow) { if(newWindow.focus) { newWindow.focus(); return false; } }
     newWindow = null;
   });
+
+  $('#navigation ol a').click(function(e) {
+    var section = $($(this).attr('href'));
+    var scroll = section.offset().top - 100;
+    $('html,body').css({scrollTop: scroll});
+    //e.preventDefault();
+  });
+
+  $('.email_address').each(function() {
+    var address     = $(this).text().replace(' [at] ', '@').replace(' [dot] ', '.');
+    var emailLink   = '<a href="mailto:' + address + '">' + address + '</a>';
+
+    $(this).empty().append(emailLink);
+  });
+
 });
