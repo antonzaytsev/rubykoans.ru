843ff572de9e81574e1f13f60ae21030f8c8a550
Jerry Nummi <jerry@edgecase.com>
2010-08-24 14:19:51 -0400

    Minor UI tweaks. Randomized display  of editors.

--- rubykoans.rb
+++ rubykoans.rb
@@ -8,10 +8,24 @@ end
 
 get '/' do
   @page = 'nix'
+  @editors = [
+    {:name => 'Vim',      :url => 'http://code.google.com/p/macvim'},
+    {:name => 'Emacs',    :url => 'http://homepage.mac.com/zenitani/emacs-e.html'},
+    {:name => 'TextMate', :url => 'http://macromates.com'},
+    {:name => 'RubyMine', :url => 'http://www.jetbrains.com/ruby'},
+    {:name => 'jEdit',    :url => 'http://www.jedit.org'}
+  ]
+
   haml :index
 end
 
 get '/windows' do
   @page = 'windows'
+  @editors = [
+    {:name => 'E Text Editor', :url => 'http://code.google.com/p/macvim'},
+    {:name => 'RubyMine',      :url => 'http://www.jetbrains.com/ruby'},
+    {:name => 'jEdit',         :url => 'http://www.jedit.org'},
+  ]
+
   haml :windows
 end
