1d52cb867aaf55567a70aab694daabc439f519bf
Anton Zaycev <antonzaycev55@gmail.com>
2011-03-18 17:01:01 +0600

    added translations, made orgonized structure, now all blocks are determined in rubykoans.rb file

--- rubykoans.rb
+++ rubykoans.rb
@@ -6,6 +6,8 @@ not_found do
   haml :error
 end
 
+
+
 get '/' do
   @page = 'nix'
   @editors = [
@@ -14,6 +16,18 @@ get '/' do
     {:name => 'TextMate', :url => 'http://macromates.com'},
     {:name => 'RubyMine', :url => 'http://www.jetbrains.com/ruby'},
     {:name => 'jEdit',    :url => 'http://www.jedit.org'}
+  ]
+
+  @blocks = [
+    {:name => 'introduction', :title => 'вступление'},
+    {:name => 'structure', :title => 'структура'},
+    {:name => "installing_ruby_on_#{@page}", :title => 'установка ruby', :id => "installing_ruby"},
+    {:name => "what_editors_#{@page}", :title => 'какие редакторы я могу использовать?', :id => "what_editors"},
+    {:name => "the_path_#{@page}", :title => 'путь к просвещению', :id => "the_path"},
+    {:name => 'authors', :title => 'авторы'},
+    {:name => 'cc', :title => 'лицензия'},
+    {:name => 'inspiration', :title => 'вдохновители'},
+    {:name => 'other_resources', :title => 'Дополнительные ресурсы'}
   ]
 
   haml :index
@@ -25,6 +39,18 @@ get '/windows' do
     {:name => 'E Text Editor', :url => 'http://www.e-texteditor.com'},
     {:name => 'RubyMine',      :url => 'http://www.jetbrains.com/ruby'},
     {:name => 'jEdit',         :url => 'http://www.jedit.org'},
+  ]
+
+  @blocks = [
+    {:name => 'introduction', :title => 'вступление'},
+    {:name => 'structure', :title => 'структура'},
+    {:name => "installing_ruby_on_#{@page}", :title => 'установка ruby', :id => "installing_ruby"},
+    {:name => "what_editors_#{@page}", :title => 'какие редакторы я могу использовать?', :id => "what_editors"},
+    {:name => "the_path_#{@page}", :title => 'путь к просвещению', :id => "the_path"},
+    {:name => 'authors', :title => 'авторы'},
+    {:name => 'cc', :title => 'лицензия'},
+    {:name => 'inspiration', :title => 'вдохновители'},
+    {:name => 'other_resources', :title => 'other_resources'}
   ]
 
   haml :windows
